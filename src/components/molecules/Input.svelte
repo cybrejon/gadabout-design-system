799fd3af4ba3aae98541a72b745c25c597e5db84
cybrejon <cybrejon.senpai@gmail.com>
2026-04-29 04:33:03 +0800

    chore: properly promote breadcrumb and input components into molecules

---
+++ src/components/molecules/Input.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	interface Props {
+		value?: string;
+		placeholder?: string;
+		label?: string;
+		label_bottom?: string;
+		icon?: string;
+		type?: string;
+	}
+	let {
+		value = '',
+		placeholder = 'Enter text',
+		label = '',
+		label_bottom = '',
+		icon,
+		type = 'text'
+	}: Props = $props();
+	import Label from '../atoms/Label.svelte';
+</script>
+
+<div class="flex flex-col gap-1">
+	{#if label}
+		<Label>
+			{label}
+		</Label>
+	{/if}
+	{#if icon}
+		<div class="relative">
+			<span class="absolute top-3.75 left-4 {icon}"></span>
+			<input
+				class="rounded-xl border border-2 bg-card py-3 pr-4 pl-10 text-sm placeholder:text-sm placeholder:text-foreground-alt focus-visible:outline-primary"
+				{type}
+				bind:value
+				{placeholder}
+			/>
+		</div>
+	{:else}
+		<input
+			class="rounded-xl border border-2 bg-card px-4 py-3 text-sm placeholder:text-sm placeholder:text-foreground-alt focus-visible:outline-primary"
+			{type}
+			bind:value
+			{placeholder}
+		/>
+	{/if}
+	{#if label_bottom}
+		<Label>
+			{label_bottom}
+		</Label>
+	{/if}
+</div>
--- src/components/stories/Breadcrumb.stories.svelte
+++ src/components/stories/Breadcrumb.stories.svelte
@@ -1,11 +1,11 @@
 <script module>
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 
-	import Breadcrumb from '../atoms/Breadcrumb.svelte';
+	import Breadcrumb from '../molecules/Breadcrumb.svelte';
 	import ContainerDecorator from '../utils/ContainerDecorator.svelte';
 
 	const { Story } = defineMeta({
-		title: 'Component/Atoms/Navigation/Breadcrumb',
+		title: 'Component/Molecules/Navigation/Breadcrumb',
 		component: Breadcrumb,
 		tags: ['autodocs'],
 		// @ts-expect-error: seems to be a bug => https://github.com/storybookjs/storybook/issues/29951
