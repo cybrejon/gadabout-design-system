3bbfc4e53ac4cea69fb0ee860674ce548bade28b
cybrejon <cybrejon.senpai@gmail.com>
2026-04-28 18:45:08 +0800

    feature: add custom color capability for Pill component + documentation

--- src/components/atoms/Pill.svelte
+++ src/components/atoms/Pill.svelte
@@ -9,8 +9,9 @@
 		children: Snippet;
 		variant?: PillVariants;
 		href?: string;
+		class?: string;
 	}
-	let { children, variant = 'normal', href }: Props = $props();
+	let { children, variant = 'normal', href, class: className }: Props = $props();
 
 	let styles: Record<PillVariants, string> = {
 		normal: 'bg-primary text-primary-foreground active:text-primary-foreground/80',
@@ -27,7 +28,7 @@
 		{href}
 		class="inline-flex cursor-pointer flex-nowrap items-center justify-center gap-2 rounded-full px-2.5 pt-1 pb-0.5 text-xxs font-semibold outline-offset-2 transition-colors {styles[
 			variant
-		]}"
+		]} {className}"
 	>
 		{@render children()}
 	</Button.Root>
@@ -35,7 +36,7 @@
 	<div
 		class="pointer-events-none inline-flex flex-nowrap items-center justify-center gap-2 rounded-full border-transparent px-2.5 pt-1 pb-0.5 text-xxs font-semibold outline-offset-2 transition-colors {styles[
 			variant
-		]}"
+		]} {className}"
 	>
 		{@render children()}
 	</div>
