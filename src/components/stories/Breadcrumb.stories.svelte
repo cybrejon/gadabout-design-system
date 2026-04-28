76f21ce618e4e2e3e979eaed222b522afc88073c
cybrejon <cybrejon.senpai@gmail.com>
2026-04-29 03:00:42 +0800

    feature: add breadcrumb component and documentation

---
+++ src/components/stories/Breadcrumb.stories.svelte
@@ -0,0 +1,35 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Breadcrumb from '../atoms/Breadcrumb.svelte';
+	import ContainerDecorator from '../utils/ContainerDecorator.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Component/Atoms/Navigation/Breadcrumb',
+		component: Breadcrumb,
+		tags: ['autodocs'],
+		// @ts-expect-error: seems to be a bug => https://github.com/storybookjs/storybook/issues/29951
+		decorators: [() => ContainerDecorator]
+	});
+</script>
+
+<Story
+	name="Main"
+	args={{
+		items: [
+			{
+				name: 'Home',
+				link: '/'
+			},
+			{
+				name: '[Leads] Opal Camidge Way',
+				link: '/'
+			},
+			{
+				name: 'Reports',
+				link: '/',
+				active: true
+			}
+		]
+	}}
+/>
