82196af04b84bd6cffbc7e9b46d5d3743f3db4b7
cybrejon <cybrejon.senpai@gmail.com>
2026-05-07 06:27:33 +0800

    chore: reduce input component dimensions

--- src/components/molecules/Input.svelte
+++ src/components/molecules/Input.svelte
@@ -35,7 +35,8 @@
 	<div>
 		<div class="relative">
 			{#if icon}
-				<span class="absolute top-3.75 left-4 {icon} pointer-events-none text-foreground/50"></span>
+				<span class="absolute top-2.75 left-3.5 w-4 {icon} pointer-events-none text-foreground/50"
+				></span>
 			{/if}
 			<input
 				{id}
@@ -43,8 +44,8 @@
 				bind:value
 				{placeholder}
 				class={[
-					'w-full rounded-xl border-2 py-3 text-sm outline-3 outline-offset-0 outline-transparent transition-colors placeholder:text-sm focus-visible:ring-transparent',
-					icon ? 'pr-4 pl-10' : 'px-4',
+					'w-full rounded-xl border-2 py-2 text-sm outline-3 outline-offset-0 outline-transparent transition-colors placeholder:text-sm focus-visible:ring-transparent',
+					icon ? 'pr-2 pl-9' : 'px-3',
 					invalid
 						? 'border-destructive/70 bg-destructive/5 text-destructive saturate-80 placeholder:text-destructive focus-visible:outline-destructive/20 focus-visible:saturate-100'
 						: 'focus: border bg-card placeholder:text-foreground-alt focus-visible:border-primary/50 focus-visible:outline-primary/10',
