3a1b64479967968a5340fc4479de8f664bde7e81
cybrejon <cybrejon.senpai@gmail.com>
2026-05-06 21:44:17 +0800

    chore: add missing attribute for input component sub-label

--- src/components/molecules/Input.svelte
+++ src/components/molecules/Input.svelte
@@ -32,30 +32,32 @@
 		</Label>
 	{/if}
 
-	<div class="relative">
-		{#if icon}
-			<span class="absolute top-3.75 left-4 {icon} pointer-events-none"></span>
-		{/if}
-		<input
-			{id}
-			{type}
-			bind:value
-			{placeholder}
-			class={[
-				'w-full rounded-xl border-2 py-3 text-sm outline-3 outline-offset-0 outline-transparent transition-colors placeholder:text-sm focus-visible:ring-transparent',
-				icon ? 'pr-4 pl-10' : 'px-4',
-				invalid
-					? 'border-destructive/70 bg-destructive/5 text-destructive saturate-80 placeholder:text-destructive focus-visible:outline-destructive/20 focus-visible:saturate-100'
-					: 'focus: border bg-card placeholder:text-foreground-alt focus-visible:border-primary/50 focus-visible:outline-primary/10',
-				className
-			]}
-			{...rest}
-		/>
-	</div>
+	<div>
+		<div class="relative">
+			{#if icon}
+				<span class="absolute top-3.75 left-4 {icon} pointer-events-none"></span>
+			{/if}
+			<input
+				{id}
+				{type}
+				bind:value
+				{placeholder}
+				class={[
+					'w-full rounded-xl border-2 py-3 text-sm outline-3 outline-offset-0 outline-transparent transition-colors placeholder:text-sm focus-visible:ring-transparent',
+					icon ? 'pr-4 pl-10' : 'px-4',
+					invalid
+						? 'border-destructive/70 bg-destructive/5 text-destructive saturate-80 placeholder:text-destructive focus-visible:outline-destructive/20 focus-visible:saturate-100'
+						: 'focus: border bg-card placeholder:text-foreground-alt focus-visible:border-primary/50 focus-visible:outline-primary/10',
+					className
+				]}
+				{...rest}
+			/>
+		</div>
 
-	{#if label_bottom}
-		<Label class="opacity-70">
-			{label_bottom}
-		</Label>
-	{/if}
+		{#if label_bottom}
+			<Label for={id} class="font-light opacity-70">
+				{label_bottom}
+			</Label>
+		{/if}
+	</div>
 </div>
