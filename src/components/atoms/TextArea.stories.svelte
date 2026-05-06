736261a944f359c11ad55db0a822d83c6b4969de
cybrejon <cybrejon.senpai@gmail.com>
2026-05-06 23:13:05 +0800

    refactor: enhanced textarea component and added documentation

---
+++ src/components/atoms/TextArea.stories.svelte
@@ -0,0 +1,53 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import TextArea from './TextArea.svelte';
+	import ContainerDecorator from '../utils/ContainerDecorator.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Component/Atoms/Forms/TextArea',
+		component: TextArea,
+		parameters: {
+			docs: {
+				controls: {
+					exclude: ['placeholder']
+				}
+			}
+		},
+		tags: ['autodocs'],
+		// @ts-expect-error: seems to be a bug => https://github.com/storybookjs/storybook/issues/29951
+		decorators: [() => ContainerDecorator]
+	});
+</script>
+
+<Story
+	name="Normal"
+	args={{
+		placeholder: 'placeholder'
+	}}
+/>
+
+<Story
+	name="Label"
+	args={{
+		placeholder: 'placeholder',
+		label: 'Label'
+	}}
+/>
+
+<Story
+	name="CombinedLabels"
+	args={{
+		placeholder: 'placeholder',
+		label: 'Label',
+		label_bottom: 'Bottom label'
+	}}
+/>
+
+<Story
+	name="Invalid"
+	args={{
+		invalid: true,
+		placeholder: 'invalid value'
+	}}
+/>
--- src/components/atoms/TextArea.svelte
+++ src/components/atoms/TextArea.svelte
@@ -1,52 +1,56 @@
 <script lang="ts">
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
 	import Label from './Label.svelte';
 
-	interface TextAreaSize {
-		rows?: number;
-		cols?: number;
-	}
-
-	interface Props {
+	interface Props extends HTMLTextareaAttributes {
 		value?: string;
-		placeholder?: string;
-		id?: string;
-		name?: string;
-		size?: TextAreaSize;
 		label?: string;
 		label_bottom?: string;
-		class?: string;
+		invalid?: boolean;
+		id?: string;
 	}
 
 	let {
-		value = '',
-		id,
-		name,
-		size = { rows: 5, cols: 53 },
-		label = 'Label',
+		value = $bindable(''),
+		id = crypto.randomUUID(),
+		label = '',
 		label_bottom = '',
-		placeholder = 'Placeholder',
-		class: className
+		placeholder = 'Enter text...',
+		invalid = false,
+		rows = 5,
+		cols = 53,
+		class: className,
+		...rest
 	}: Props = $props();
 </script>
 
 <div class="flex flex-col gap-1">
 	{#if label}
-		<Label for={name}>{label}</Label>
+		<Label for={id}>
+			{label}
+		</Label>
 	{/if}
 
 	<div>
 		<textarea
-			{placeholder}
-			form={id}
-			class="rounded-xl border border-2 bg-card p-3 text-sm text-foreground outline-3 outline-offset-0 outline-transparent placeholder:text-foreground-alt focus-visible:border-primary/50 focus-visible:ring-transparent focus-visible:outline-primary/10 {className}"
 			{id}
-			{name}
-			rows={size.rows}
-			cols={size.cols}>{value}</textarea
-		>
-
+			bind:value
+			{placeholder}
+			{rows}
+			{cols}
+			class={[
+				'block w-full rounded-xl border-2 p-3 text-sm outline-3 outline-offset-0 outline-transparent transition-colors placeholder:text-sm focus-visible:ring-transparent',
+				invalid
+					? 'border-destructive/70 bg-destructive/5 text-destructive saturate-80 placeholder:text-destructive focus-visible:outline-destructive/20 focus-visible:saturate-100'
+					: 'border bg-card placeholder:text-foreground-alt focus-visible:border-primary/50 focus-visible:outline-primary/10',
+				className
+			]}
+			{...rest}
+		></textarea>
 		{#if label_bottom}
-			<Label class="opacity-70" for={name}>{label_bottom}</Label>
+			<Label for={id} class="font-light opacity-70">
+				{label_bottom}
+			</Label>
 		{/if}
 	</div>
 </div>
