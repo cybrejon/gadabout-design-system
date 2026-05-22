073c3ad6a9dedba63ed5f5db309438ea4a143cf1
cybrejon <cybrejon.senpai@gmail.com>
2026-05-22 22:13:09 +0800

    refactor: bring over changes from main gadabout dashboard over

--- src/components/atoms/Label.svelte
+++ src/components/atoms/Label.svelte
@@ -1,9 +1,11 @@
 <script>
 	import { Label } from 'bits-ui';
-	let { children, class: className = '', for: for_attribute } = $props();
+
+	let { children, class: className = '', for: for_attribute, el = $bindable(null) } = $props();
 </script>
 
 <Label.Root
+	bind:ref={el}
 	for={for_attribute}
 	class="text-xs font-semibold tracking-wide text-foreground-alt {className}"
 >
