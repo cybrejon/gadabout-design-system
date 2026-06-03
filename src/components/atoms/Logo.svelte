21abee588188b1f810fceb1b6e5061cc75243e80
cybrejon <cybrejon.senpai@gmail.com>
2026-06-04 01:22:57 +0800

    feat: add logo component

---
+++ src/components/atoms/Logo.svelte
@@ -0,0 +1,12 @@
+<script>
+	/**
+	 * @typedef {Object} Props
+	 * @property {string} [class] - Additional CSS classes.
+	 * @property {any} [restProps] - Any other props to pass to the underlying image element.
+	 */
+
+	/** @type {Props} */
+	let { class: style = '', ...restProps } = $props();
+</script>
+
+<img src="/images/logo.svg" alt="Gadabout" class={style} {...restProps} />
