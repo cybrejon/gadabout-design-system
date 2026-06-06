6a7fc4d2b9413d80bed36401899c196ca571cc4a
cybrejon <cybrejon.senpai@gmail.com>
2026-06-07 05:13:20 +0800

    feat: add dedicated link component

---
+++ src/components/atoms/Link.stories.svelte
@@ -0,0 +1,43 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Link from './Link.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Atoms/Link',
+		component: Link,
+		argTypes: {
+			variant: {
+				control: { type: 'select' },
+				options: ['hover', 'ghost', 'none']
+			}
+		}
+	});
+</script>
+
+<Story name="Default">
+	<Link href="#">Default Link</Link>
+</Story>
+
+<Story name="Hover Underline">
+	<Link href="#" variant="hover">Hover Underline Link</Link>
+</Story>
+
+<Story name="Ghost">
+	<Link href="#" variant="ghost">Ghost Link</Link>
+</Story>
+
+<Story name="With Icon Left">
+	<Link href="#">
+		<span class="icon-[lucide--link-2] size-4"></span>
+		Link with Icon
+	</Link>
+</Story>
+
+<Story name="In Paragraph">
+	{#snippet template()}
+		<p class="text-base leading-relaxed">
+			This is a paragraph of text that contains a <Link href="#">link</Link> to demonstrate how it aligns
+			with the surrounding text. It should feel part of the flow.
+		</p>
+	{/snippet}
+</Story>
