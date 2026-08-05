96d5fbbb2f126152dd4d193b83c3f4b767a64141
cybrejon <cybrejon.senpai@gmail.com>
2026-08-06 00:18:07 +0800

    refactor: move input component from molecules and into atoms

---
+++ src/components/atoms/Input.stories.svelte
@@ -0,0 +1,60 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Input from './Input.svelte';
+	import ContainerDecorator from '../utils/ContainerDecorator.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Component/Molecules/Forms/Input',
+		component: Input,
+		parameters: {
+			docs: {
+				controls: {
+					exclude: ['placeholder']
+				}
+			}
+		},
+		tags: ['autodocs'],
+		decorators: [() => ContainerDecorator]
+	});
+</script>
+
+<Story
+	name="Normal"
+	args={{
+		placeholder: 'placeholder'
+	}}
+/>
+
+<Story
+	name="Label"
+	args={{
+		placeholder: 'placeholder',
+		label: 'Label'
+	}}
+/>
+
+<Story
+	name="Icon"
+	args={{
+		placeholder: 'Search',
+		icon: 'icon-[lucide--search]'
+	}}
+/>
+
+<Story
+	name="CombinedLabels"
+	args={{
+		placeholder: 'placeholder',
+		label: 'Label',
+		label_bottom: 'Bottom label'
+	}}
+/>
+
+<Story
+	name="Invalid"
+	args={{
+		invalid: true,
+		placeholder: 'invalid value'
+	}}
+/>
